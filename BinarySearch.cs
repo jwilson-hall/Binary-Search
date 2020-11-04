b1363520c2b9a6a426b9f1efec833c52bf6dc89b
jwilson-hall <40354936+jwilson-hall@users.noreply.github.com>
2020-11-04 20:45:00 +0000

    c#

---
+++ BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿namespace BinarySearch
+{
+    class BinarySearch
+    {
+        public static int binarySearch(int[] sortedList,int target)
+        {
+            if (sortedList.Length == 0) return -1;
+            int left = 0;
+            int right = sortedList.Length - 1;
+            while (left<=right)
+            {
+                int midpoint = left + (right - left) / 2;
+                if (sortedList[midpoint] == target)
+                {
+                    return midpoint;
+                }
+                else if (sortedList[midpoint] > target)
+                {
+                    right = midpoint - 1;
+                }
+                else
+                {
+                    left = midpoint + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
